3c9472e409e9a2235707bfd84320ea36df0dfa07
yangchristina <69733531+yangchristina@users.noreply.github.com>
2021-08-14 12:06:09 -0700

    draggable healthbar started

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
 /* todos
-*   percent in heart stop at 0%
 *   warning flashing lights
-*   background scripts
 *   sign in page???
+*   
 */
 
 
@@ -12,10 +11,9 @@ var s = function(sketch) {
     let col;
     let imgHeart;
     let imgWater;
-    let timeElapsed;
     let myHealth = 100;
-    let windowHeight = window.innerHeight
-
+    let windowHeight = window.innerHeight 
+    let healthX=60; let healthY = windowHeight-45; 
     sketch.preload = function() {
         let urlHeart = chrome.extension.getURL('assets/heart.png')
         imgHeart = sketch.loadImage(urlHeart);
@@ -26,7 +24,6 @@ var s = function(sketch) {
     sketch.setup = function() {
         chrome.runtime.onMessage.addListener(sketch.gotMessage);
         
-        timeElapsed = 0
         fullHealth = 144
         col = 150
 
@@ -44,44 +41,53 @@ var s = function(sketch) {
     };
 
     sketch.draw = function() {
-        
+  
 
         sketch.clear();
         console.log(myHealth)
         myHealthBar = myHealth/100 * fullHealth
-        if(col>0)
-            col = myHealth/100*180-30
 
-        sketch.HealthBar()
+        if(sketch.mouseIsPressed)
+            if(sketch.mouseX>healthX && sketch.mouseX<healthX+150 && sketch.mouseY>healthY && sketch.mouseY<healthY+30)
+                healthX = sketch.mouseX; 
+                healthY = sketch.mouseY; 
+
+        if(col>0)
+            col = myHealth/100*190-40
+            sketch.HealthBar()
         sketch.Water()
     };
     sketch.gotMessage =function(message, sender, sendResponse) {
-        
         if(myHealth >0){
             myHealth--
-            // timeElapsed += sketch.deltaTime/1000
             // myHealth -= sketch.deltaTime*0.0000167 * 500 //get rid of 500 for production
         }
+        console.log(message.txt)
     }
     
     sketch.HealthBar = function() {
          //outer rectangle
          sketch.fill(0,0,0);
- 
-         sketch.rect(60, windowHeight-45, 150, 30, 20);
+
+         sketch.rect(healthX, healthY, 150, 30, 20);
          
          //inner rectangle
          sketch.colorMode(sketch.HSB)
          sketch.fill(col, 100, 100);
-         sketch.rect(60+3, windowHeight-45+3, myHealthBar, 24, 20);
+         sketch.rect(healthX+3, healthY+3, myHealthBar, 24, 20);
          
-         //heart
-         sketch.image(imgHeart, 5,  windowHeight-57);
+         //heart, x = 5, y = windowHeight-57
+         sketch.image(imgHeart, healthX-55, healthY-12);
          sketch.fill('white')
+         sketch.textSize(sketch.width / 80);
          sketch.text(Math.floor(myHealth)+'%', 34, windowHeight-30);
     }
     sketch.Water = function(){
-        sketch.image(imgWater, 50,  windowHeight-100);
+        sketch.image(imgWater, healthX,  healthY-60);
+    }
+    sketch.BlinkingWarning = function(){
+        sketch.textSize(32)
+        sketch.text("Warning, low health", 34, windowHeight-30);
     }
 
 };
