00c6f92a61a7e53a02557d2f703bee378ffe252d
yangchristina <69733531+yangchristina@users.noreply.github.com>
2021-08-14 17:16:18 -0700

    ready to use
    Co-authored-by: xavier-c-lam <xavier-c-lam@users.noreply.github.com>

--- background.js
+++ background.js
@@ -1,97 +1,57 @@
 console.log("background running")
+let health = 100
+let newState;
+let healthRate = 1
+let activeTab;
 
+chrome.tabs.onActivated.addListener(({tabId: activeTab}) => {
+    let msg = {
+        myHealth: health
+    }
+    console.log("hi: "+health)
+    console.log(activeTab)
+    chrome.tabs.sendMessage(activeTab, msg);
+})
 chrome.runtime.onInstalled.addListener(() => {
     console.log('onInstalled...');
     // create alarm after extension is installed / upgraded
     chrome.alarms.create('refresh', { periodInMinutes: 1 });
 });
-  
-chrome.alarms.onAlarm.addListener((alarm) => {
-    console.log(alarm.name); // refresh
-    helloWorld();
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request)
+    if(request == "water"){
+        health += 10
+        if(health >100) 
+            health = 100
+        console.log(health)
+    }else if(request =='park'){
+        if(healthRate == 2){
+            heathRate = 1
+        }else{healthRate = 2}
+    }
+    sendResponse(health)
 });
 
-function helloWorld(tab) {
+chrome.alarms.onAlarm.addListener((alarm) => {
+
+    chrome.idle.queryState(60, function(newState) {
+        if(newState =="active")
+            if(health>0)
+                health-= 5
+        else{
+            health+=5 * healthRate
+            if(health>100) health=100
+        }console.log(newState)
+        sendHealth();
+    })
+});
+
+function sendHealth() {
     let msg = {
-        txt: "hello"
+        myHealth: health
     }
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id, msg);  
-    });
-    console.log("Hello, world!");
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(activeTab, msg);  
+    // });
+    console.log("Hello, world!: "+health);
 }
-
-
-
-
-
-// let tab1;
-// chrome.tabs.getSelected(null, function(tab){
-//     console.log(tab);
-//     tab1 = tab
-//     let msg = {
-//         txt: "hello"
-//     }
-//     chrome.tabs.sendMessage(tab1.id, msg)
-// });
-// // console.log(tab1)
-// chrome.browserAction.onClicked.addListener(buttonClicked)
-
-// function buttonClicked(tab) {
-//     let msg = {
-//         txt: "hello"
-//     }
-//     chrome.tabs.sendMessage(tab.id, msg)
-// }
-
-
-
-
-
-
-
-
-// var s = function(sketch) {
-    // console.log(TAB)
-    // console.log("in p5")
-    // let timeElapsed;
-    // let myHealth;
-    // let fullHealth = 144
-  
-    // sketch.setup = function() { 
-        // console.log("in set-up")  
-        // timeElapsed = 0
-        // myHealth = 100
-
-        // sketch.draw();
-    // };
-
-    // sketch.draw = function() {
-        // console.log("in draw") 
-        // let msg = {
-        //     txt: "hello"
-        // }
-        // chrome.tabs.sendMessage(tab.id, msg)
-        // sketch.clear();
-        // if(myHealth >=0){
-        //     timeElapsed += sketch.deltaTime/1000
-        //     myHealth -= sketch.deltaTime*0.0000167 * 500 //get rid of 500 for production
-        //     myHealthBar = myHealth/100 * fullHealth
-        //     console.log(timeElapsed)
-        //     console.log("my health: "+ myHealth)
-        //     if(col>0)
-        //         col = myHealth/100*180-30
-        // }
-        // sketch.HealthBar()
-        // sketch.Water()
-//     };
-    
-//     sketch.HealthBar = function() {
-
-//     }
-//     sketch.Water = function(){
-//     }
-
-// };
-
-// var myp5 = new p5(s);
--- sketch.js
+++ sketch.js
@@ -6,63 +6,76 @@
 
 
 var s = function(sketch) {
-    
+    let blinkerCount
     let c
     let col;
     let imgHeart;
     let imgWater;
-    let myHealth = 100;
+    let imgPark;
+    let myHealth;
     let windowHeight = window.innerHeight 
     let healthX=60; let healthY = windowHeight-45; 
+    let value = 0
+    let showIcons = false
+    let walkingMode = false
+
+
     sketch.preload = function() {
         let urlHeart = chrome.extension.getURL('assets/heart.png')
         imgHeart = sketch.loadImage(urlHeart);
         let urlWater = chrome.extension.getURL('assets/water.png')
         imgWater = sketch.loadImage(urlWater);
+        let urlPark = chrome.extension.getURL('assets/park.png')
+        imgPark = sketch.loadImage(urlPark);
+
     }
+
+
     
     sketch.setup = function() {
+        myHealth = 100
         chrome.runtime.onMessage.addListener(sketch.gotMessage);
         
         fullHealth = 144
         col = 150
+        blinkerCount = 0
+        blinker = true
+
 
         imgHeart.resize(55, 55)
         imgWater.resize(55, 70)
+        imgPark.resize(55, 55)
 
         c = sketch.createCanvas(window.innerWidth, windowHeight);
         c.position(0, 0, 'fixed');
         c.style('pointer-events', 'none');
         c.style('z-index', '999');
 
+        sketch.textStyle(sketch.BOLD);
         sketch.textFont('inconsolata');
         sketch.textSize(sketch.width / 80);
         sketch.textAlign(sketch.CENTER, sketch.CENTER);
     };
 
     sketch.draw = function() {
-  
+        c.style('pointer-events', 'none');
 
         sketch.clear();
         console.log(myHealth)
         myHealthBar = myHealth/100 * fullHealth
 
-        if(sketch.mouseIsPressed)
-            if(sketch.mouseX>healthX && sketch.mouseX<healthX+150 && sketch.mouseY>healthY && sketch.mouseY<healthY+30)
-                healthX = sketch.mouseX; 
-                healthY = sketch.mouseY; 
-
+          
         if(col>0)
             col = myHealth/100*190-40
-            sketch.HealthBar()
+        sketch.HealthBar()
         sketch.Water()
+        sketch.Park()
+        if(myHealth<15) sketch.BlinkingWarning()
+
     };
-    sketch.gotMessage =function(message, sender, sendResponse) {
-        if(myHealth >0){
-            myHealth--
-            // myHealth -= sketch.deltaTime*0.0000167 * 500 //get rid of 500 for production
-        }
-        console.log(message.txt)
+    sketch.gotMessage =function(msg, sender, sendResponse) {
+        myHealth = msg.myHealth
+        console.log(myHealth)
     }
     
     sketch.HealthBar = function() {
@@ -80,16 +93,68 @@ var s = function(sketch) {
          sketch.image(imgHeart, healthX-55, healthY-12);
          sketch.fill('white')
          sketch.textSize(sketch.width / 80);
-         sketch.text(Math.floor(myHealth)+'%', 34, windowHeight-30);
+         sketch.text(Math.floor(myHealth)+'%', 34, healthY+15);
     }
     sketch.Water = function(){
-        sketch.image(imgWater, healthX,  healthY-60);
+        if(showIcons)
+            sketch.image(imgWater, healthX-60,  healthY-70);
     }
+
+    sketch.Park = function(){
+        if(showIcons)
+            sketch.image(imgPark, healthX-55,  healthY-120);
+
+        if(walkingMode)
+            sketch.goneOutside()
+
+    }
+
+    sketch.goneOutside = function(){
+        sketch.textSize(100)
+        sketch.fill('black')
+        sketch.text("AFK mode: gone outside", 0, sketch.height/2, sketch.width);
+    }
+
+
     sketch.BlinkingWarning = function(){
-        sketch.textSize(32)
-        sketch.text("Warning, low health", 34, windowHeight-30);
+        sketch.textSize(50)
+        sketch.fill('red')
+        if(blinkerCount <= 30){
+            sketch.text("Warning, low health", 0, sketch.height/2, sketch.width);
+        }else if(blinkerCount > 60){
+            blinkerCount = -1
+        }
+        blinkerCount++
+    }
+    sketch.mouseDragged=function(event){
+        if(sketch.mouseX>healthX && sketch.mouseX<healthX+150 && sketch.mouseY>healthY && sketch.mouseY<healthY+30){
+            // healthX = sketch.mouseX; 
+            c.style('pointer-events', 'auto');
+            healthY += event.movementY;  
+        }
     }
 
-};
+    sketch.mouseClicked = function(event){
+        if(sketch.mouseX>healthX-55 && sketch.mouseX<healthX-5 && sketch.mouseY>healthY-10 && sketch.mouseY<healthY+40){
+            c.style('pointer-events', 'auto');
+            showIcons = !showIcons
+        }
+        if(sketch.mouseX>healthX-55 && sketch.mouseX<healthX-5 && sketch.mouseY>healthY-120 && sketch.mouseY<healthY-70){
+            c.style('pointer-events', 'auto');
+            window.open('https://www.google.com/maps?q=parks+near+me');
+            walkingMode = !walkingMode
+            chrome.runtime.sendMessage('park', (response)=> {
+                myHealth = response
+            });
+        }
+        if(sketch.mouseX>healthX-55 && sketch.mouseX<healthX-5 && sketch.mouseY>healthY-70 && sketch.mouseY<healthY-10){
+            c.style('pointer-events', 'auto');
+            chrome.runtime.sendMessage('water', (response)=> {
+                myHealth = response
+            }); 
+        }
+    }
 
+    
+}
 var myp5 = new p5(s);
